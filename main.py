dd756080aff33afcb70f95cd93f52bf55aaa28fb
Jasurbek-Tursunov <jasurbek3124@icloud.com>
2022-06-13 10:31:26 +0500

    Add token -> main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import logging
  
 from aiogram import Bot, Dispatcher, executor, types
  
-API_TOKEN = 'BOT TOKEN HERE'
+API_TOKEN = '5242807366:AAF8pFlM3ecs83tdb-o3ExgrOhUzk36k9Uc'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -21,7 +21,7 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply("Assalomu Aleykum")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
